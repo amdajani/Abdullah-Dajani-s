3ff67863162f863aab14360a79801209ed9e8717
Abdullah Dajani <dajani.abdullah.majid@gmail.com>
2024-06-04 16:25:33 -0400

    Update styles and and resume.pdf

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customNavy: '#000035', // Replace with your specific color
+      },
+    },
   },
   plugins: [],
 }
